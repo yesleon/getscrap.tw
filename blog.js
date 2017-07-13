49e5b1374e40d12884de34c5113d04f4d1c8451b
Li-Heng Hsu <yesleon@me.com>
2017-07-13 18:35:26 +0800

    Embedded a Tumblr blog.

---
+++ blog.js
@@ -0,0 +1,25 @@
+let posts = tumblr_api_read['posts'];
+
+for (var index = 0; index < posts.length; index++) {
+	let post = posts[index];
+	let unixTimestamp = post['unix-timestamp'];
+	let date = new Date(unixTimestamp * 1000);
+	let dd = date.getDate();
+	let mm = date.getMonth() + 1;
+	let yyyy = date.getFullYear();
+	let formattedDateString = yyyy + '/' + mm + '/' + dd;
+	let title = post['regular-title'];
+	let body = post['regular-body'];
+	
+	let string = ''
+	+ '<article class="blogPost"><header><h1>' + title + '</h1>'
+	+ 'Li-Heng on ' + formattedDateString
+	+ '</header>'
+	
+	+ body
+	
+	+ '</article>';
+	
+	document.getElementById("blogPosts").innerHTML += string;
+}
+
